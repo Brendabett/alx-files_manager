fa4b289280a1b951c368f75299aaa570479b0423
brendabett <brendabett33@gmail.com>
2023-03-06 16:30:56 +0000

    API

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-const express = require('express');
+import express from 'express';
+import router from './routes';
 
 const app = express();
+const port = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(router);
 
-// Load routes from routes/index.js
-const routes = require('./routes');
-
-app.use('/', routes);
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
