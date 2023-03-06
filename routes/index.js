f34ae37716b3cba729138ebb96538d46174c33dc
brendabett <brendabett33@gmail.com>
2023-03-06 16:34:49 +0000

    API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-const express = require('express');
+import {
+  Router,
+} from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 
-const router = express.Router();
-const AppController = require('../controllers/AppController');
+const router = Router();
 
+// check status and stats of db
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-module.exports = router;
+// connect and disconnect user
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+// upload files
+router.post('/files', FilesController.postUpload);
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+
+// publish and unpublish
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+// user
+router.post('/users', UsersController.postNew);
+router.get('/users/me', UsersController.getMe);
+
+// file content
+router.get('/files/:id/data', FilesController.getFile);
+
+export default router;
